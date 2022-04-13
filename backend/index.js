fd12b77dafdeef2852ec167151aca7a92ac1c49a
Jakob <jakobquantum@tuta.io>
2022-04-13 11:42:55 +0200

    post data complete

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,24 @@
 const database = require("./database");
 const PORT = 4000;
 const app = require("express")();
+const cors = require('cors');
+const express = require("express")
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+app.all('/users', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],);
+  next();
+ });
+
 
 app.get("/", async (req, res) => {
   return res.send("Hello world");
@@ -19,7 +37,55 @@ app.listen(PORT, (err) => {
   console.log(`Backend started - listening on port ${PORT}`);
 });
 
+app.post("/users", async (req,res)=> {
+  console.log(req.body);
+  
+
+  
+  
+  const db = await database();}
+  
+  
+  //db.serialize(()=>{
+    //db.run('INSERT INTO emp(id,name) VALUES(?,?)', [req.params.id, req.params.name], function(err) {
+      //if (err) {
+      //  return console.log(err.message);
+     // }
+     // console.log("New user has been added");
+     // res.send("New employee has been added into the database with ID = "+req.params.id+ " and Name = "+req.params.name);
+   // });
+//});
+//});
+
+
+//app.get('/users', function(req,res){
+//  db.serialize(()=>{
+//    db.run('UPDATE emp SET name = ? WHERE id = ?', [req.params.name,req.params.id], function(err){
+//      if(err){
+//        res.send("Error encountered while updating");
+//        return console.error(err.message);
+//      }
+//      res.send("Entry updated successfully");
+//      console.log("Entry updated successfully");
+//    });
+//  });
+//});
+
+
+//app.get('/users', function(req,res){
+//  db.serialize(()=>{
+//    db.run('DELETE FROM emp WHERE id = ?', req.params.id, function(err) {
+//      if (err) {
+//        res.send("Error encountered while deleting");
+//        return console.error(err.message);
+//      }
+//      res.send("Entry deleted");
+//      console.log("Entry deleted");
+//    });
+//  });
+//});
 
 
 
 
+)
--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -1,23 +1,74 @@
 import "./Form.css";
+import { useState} from "react";
+
+
+const initialUser = {
+  firstname: "",
+  lastname: "",
+  country: "",
+  };
 
 function Form() {
-  function onSubmit(e) {
-    e.preventDefault();
-    console.log("Form.jsx -> onSubmit");
-  }
+  
+  
+  
+  
+  const [user, setUser] = useState(initialUser)
+  
+  
+  const handleChange = (event) => {
+    
+    const { name, value } = event.target;
+
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert('form submitted!' +'\n first name: ' + user.firstname + '\n last name: '
+    + user.lastname + '\n country: ' + user.country
+    )
+  
+    fetch("http://localhost:4000/users",{
+      method: 'POST',
+      headers:{ 'Content-Type': 'application/json; charset=utf-8' },
+      body: JSON.stringify(user)
+    }).then((response) => response.json())
+      .catch(error => console.error('Error: ', error))
+    
+      .then(response => console.log('Success:', response));
+    
+  
+    }
 
   return (
-    <form onSubmit={onSubmit} className="form">
+    <form onSubmit ={handleSubmit} className="form">
       <label htmlFor="name">Firstname</label>
-      <input type="text" id="name" />
+      <input type="text" name="firstname" 
+      value = {user.firstname}
+      onChange={handleChange}
+      
+      />
 
       <label htmlFor="name">Lastname</label>
-      <input type="text" id="name" />
+      <input type="text" name="lastname" 
+      value = {user.lastname}
+      onChange={handleChange}
+      
+      
+      />
 
       <label htmlFor="name">Country</label>
-      <input type="text" id="name" />
+      <input type="text" name="country" 
+      value = {user.country}
+      onChange={handleChange}
+      />
 
-      <button>Submit form</button>
+      <button type = "submit">Submit form</button>
     </form>
   );
 }
